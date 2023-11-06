88c5c64b689873c448cc08781393eaf0f0366c21
trollord <mkasangottuwar@clarapath.com>
2023-11-06 20:14:08 +0530

    created a branch this branch has the card for the news

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
+import NewsCard from "@/newsCard/news";
+
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <NewsCard />  
   )
 }
