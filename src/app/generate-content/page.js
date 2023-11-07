3dc8731943713e77596e4045d4594a3e715f732f
Rishabh Sinha <sinharishabh2002@gmail.com>
2023-11-07 21:31:47 +0530

    post-text-card

--- src/app/generate-content/page.js
+++ src/app/generate-content/page.js
@@ -1,9 +1,13 @@
 import Header from "@/components/header";
+import PostText from "@/components/Generate-Content/post-text";
 
 export default function GenerateContent() {
   return (
     <div>
         <Header header="Create Content"/>
+        <div className="post-text float-right">
+        <PostText/>
+        </div>
     </div>
   )
 }
---
+++ src/components/Generate-Content/post-text.js
@@ -0,0 +1,7 @@
+export default function PostText (){
+    return(
+        <div className="post-text">
+            Hello
+        </div>
+    );
+};
