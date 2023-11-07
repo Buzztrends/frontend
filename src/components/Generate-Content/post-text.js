442b66b9dbbe751ee9715a990dd357273c5d8335
Rishabh Sinha <sinharishabh2002@gmail.com>
2023-11-08 02:24:52 +0530

    Post edit component

--- src/components/Generate-Content/post-text.js
+++ src/components/Generate-Content/post-text.js
@@ -1,27 +1,95 @@
+import { BsFillEyeFill, BsPencilSquare } from "react-icons/bs";
 import TabComponent from "./tab-component";
 
 export default function PostText() {
   return (
+    <div>
+      <div className="content-head m-4 text-xl text-indigo-800">
+        Instagram :
+      </div>
       <div className="block rounded-lg border-black border md:w-1/2 m-4 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
         <div className="border-b text-indigo-900 border-neutral-900 px-6 py-3">
           Post Text
         </div>
-        <div className="px-6 py-4">
-
-        <TabComponent items={PostTabs} />
+        <div className="px-6 py-4 flex justify-between">
+          <TabComponent items={PostTabs} />
         </div>
       </div>
-    // </div>
+    </div>
   );
 }
 
+// var post_text = "Content of Preview";
+
 const PostTabs = [
   {
-    tab: <div>Preview</div>,
-    content: <div>Content of Preview</div>,
+    tab: (
+      <div className="flex">
+        <div className="m-2 text-lg">
+          <BsFillEyeFill />
+        </div>
+        <div className="m-1">Preview</div>
+      </div>
+    ),
+    content: (
+      <div className="p-2 text-sm">
+        Title: "Art Meets Nutrition with our #SmoothieBowls! ",
+        <br />
+        <br />
+        Do you love a healthy, delicious treat that's as pleasing to the eye as
+        it is to the palate? If you've been scrolling Instagram lately, you've
+        likely spotted the viral #SmoothieBowl trend. It's not just about
+        sipping your nutrients anymore; it's about creating a masterpiece in a
+        bowl!
+        <br />
+        <br />
+        Whole Foods is thrilled to be part of this exciting trend. We've got all
+        the organic, wholesome ingredients you need to make your very own
+        Insta-worthy smoothie bowl. A € Our shelves are filled with vibrant
+        fruits, crunchy granola, creamy yogurts, and superfood add-ons like chia
+        seeds and goji berries.
+        <br />
+        <br />
+        What's more, we love seeing your creations! So next time
+        <br />
+        <br />
+        #WholeFoodsSmoothieBowl #HealthyLiving #FoodAsArt
+      </div>
+    ),
   },
   {
-    tab: <div>Edit</div>,
-    content: <div>Content of Edit</div>,
+    tab: (
+        <div className="flex">
+          <div className="m-2 text-lg">
+            <BsPencilSquare />
+          </div>
+          <div className="m-1">Edit</div>
+        </div>
+      ),
+    content: (
+      <div className="border p-2 text-sm bg-gray-200">
+        Title: "Art Meets Nutrition with our #SmoothieBowls! ",
+        <br />
+        <br />
+        Do you love a healthy, delicious treat that's as pleasing to the eye as
+        it is to the palate? If you've been scrolling Instagram lately, you've
+        likely spotted the viral #SmoothieBowl trend. It's not just about
+        sipping your nutrients anymore; it's about creating a masterpiece in a
+        bowl!
+        <br />
+        <br />
+        Whole Foods is thrilled to be part of this exciting trend. We've got all
+        the organic, wholesome ingredients you need to make your very own
+        Insta-worthy smoothie bowl. A € Our shelves are filled with vibrant
+        fruits, crunchy granola, creamy yogurts, and superfood add-ons like chia
+        seeds and goji berries.
+        <br />
+        <br />
+        What's more, we love seeing your creations! So next time
+        <br />
+        <br />
+        #WholeFoodsSmoothieBowl #HealthyLiving #FoodAsArt
+      </div>
+    ),
   },
 ];
--- src/components/Generate-Content/tab-component.js
+++ src/components/Generate-Content/tab-component.js
@@ -1,14 +1,42 @@
-import './style.css';
+"use client";
+import "./style.css";
+import {useState, useEffect, useRef } from "react";
 
-export default function TabComponent({items}) {
-    return(
-        <div className="max-w-md flex flex-col gap-y-2 md:w-1/2">
-            <div className="bg-gray-400 rounded-lg flex justify-between items-center gap-x-2">
-                {items.map((item, index) => (
-                    <button key={index} className="outline-none w-full p-2 rounded-xl focus:ring-2 focus:bg-gradient-linear focus:text-white">{item.tab}</button>
-                ))}
-            </div>
-            
+export default function TabComponent({ items }) {
+  const firstBtnRef = useRef(null);
+  const [selectedTab, setSelectedTab] = useState(0);
+
+  useEffect(() => {
+    firstBtnRef.current.focus();
+  }, []);
+
+  return (
+    <div className="flex flex-col gap-y-2 md:w-full">
+      <div className="flex justify-between">
+        <div className="bg-gray-400 rounded-lg flex justify-between md:w-1/2 items-center gap-x-2">
+          {items.map((item, index) => (
+            <button
+              ref={index === 0 ? firstBtnRef : null}
+              key={index}
+              onClick={() => setSelectedTab(index)}
+              className={`outline-none w-full p-2 rounded-xl focus:bg-gradient-linear focus:text-white ${selectedTab === index ? 'bg-gradient-linear text-white' : ''}`}
+            >
+              {item.tab}
+            </button>
+          ))}
         </div>
-    );
-};
+        <div className="items-center mt-2">
+          <button>Edit Prompt</button>
+        </div>
+      </div>
+
+      <div>
+        {items.map((item, index) => (
+          <div className={`${selectedTab === index ? "" : "hidden"}`}>
+            {item.content}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
