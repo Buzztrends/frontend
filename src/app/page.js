15bf8f3d169f7cff6dd102b33f5f1170a05f1a7c
trollord <mkasangottuwar@clarapath.com>
2023-11-07 15:40:11 +0530

    added card component

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,14 @@
+import NewsCard from "@/components/NewsCard";
 
 
 export default function Home() {
   return (
-    <div>
-      Empty
-      </div>
+    
+    <div class="grid grid-cols-4 gap-4">       
+    <grid-item>
+    <NewsCard height={"284px"} width={"295px"} src={'/images/card.png'}  Trendscore={100} engScore={'100k'}/>
+    </grid-item>  
+    </div>
+  
   )
 }
