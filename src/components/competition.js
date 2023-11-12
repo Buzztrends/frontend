c50cbca0c35826086f2217c363b5f575bbac1933
sps-master <siddhantsharma26072002@gmail.com>
2023-11-12 18:10:22 +0530

    sidebar_aligned

--- src/components/competition.js
+++ src/components/competition.js
@@ -3,10 +3,16 @@ import Competitioncard from './competitioncard'
 
 const Competition = () => {
   return (
-    <div className='flex flex-col gap-2 p-2 border border-solid border-black'>
-      <Competitioncard />
-    </div>
-  )
-}
+    <div className='flex flex-col gap-5 '>
+        <div className='text-24 font-semibold leading-3 tracking-tighter text-font-color'>Competition</div>
+        <div className='flex flex-wrap justify-between gap-y-7'>
+            <Competitioncard />
+            <Competitioncard />
+            <Competitioncard />
+            <Competitioncard />
+        </div>
+        </div>
+)
+  }
 
 export default Competition;
