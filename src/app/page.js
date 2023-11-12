02304addac6765c963cb6593f13b253d0c66c59f
sps-master <siddhantsharma26072002@gmail.com>
2023-11-12 17:48:23 +0530

    competittion-image

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,14 @@
 import {Sidebar} from '../components/sidebar';
 import Instapost from '../components/instapost';
 import Socialmedia from '../components/socialmedia';
+import Competition from '@/components/competition';
 
 export default function Home() {
   return (
     <div className='flex'>
       <Sidebar />
-      <div className='p-5 w-4/5'>
-      <Socialmedia />
+      <div className='p-5 w-4/5 ml-auto'>
+      <Competition />
       </div>
       </div>
   )
---
+++ src/components/competition.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Competitioncard from './competitioncard'
+
+const Competition = () => {
+  return (
+    <div className='flex flex-col gap-5 '>
+        <div className='text-24 font-semibold leading-3 tracking-tighter text-font-color'>Competition</div>
+        <div className='flex flex-wrap justify-between gap-y-7'>
+            <Competitioncard />
+            <Competitioncard />
+            <Competitioncard />
+            <Competitioncard />
+        </div>
+      
+    </div>
+  )
+}
+
+export default Competition
