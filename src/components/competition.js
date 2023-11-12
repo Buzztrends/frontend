5e346a48908eb1bc8cc12538fbb155ad6fdf64df
sps-master <siddhantsharma26072002@gmail.com>
2023-11-12 18:04:52 +0530

    compcard

--- src/components/competition.js
+++ src/components/competition.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Competitioncard from './competitioncard'
 
 const Competition = () => {
   return (
     <div className='flex flex-col gap-2 p-2 border border-solid border-black'>
-      
+      <Competitioncard />
     </div>
   )
 }
---
+++ src/components/competitioncard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Competitioncard = () => {
+  return (
+  
+      <img src='/images/image 2 (1).png'></img>
+
+  )
+}
+
+export default Competitioncard
