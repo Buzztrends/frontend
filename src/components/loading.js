840f828a9bbf4cb1773ae2c86af25aa95e1831b8
Rishabh Sinha <sinharishabh2002@gmail.com>
2023-11-16 00:47:07 +0530

    generate button loader card

---
+++ src/components/loading.js
@@ -0,0 +1,41 @@
+export default function Loading() {
+  return (
+    <div className="generate-loader">
+                        <div className="flex">
+                            <div className="loader-message m-8 align-middle">Please wait while we are generating your content: </div><span className="loading align-middle loading-bars loading-lg"></span>
+                        </div>
+                        <div className="flex">
+                            <LoadingCard />
+                            <LoadingCard />
+                        </div>
+                    </div>
+  )
+}
+
+const LoadingCard = () => {
+    return (
+
+        <div className="mx-auto w-[45%] bg-white rounded shadow-lg rounded-2xl">
+            <div className="h-48 p-3 overflow-hidden bg-gray-200 animate-pulse">
+            </div>
+            <div className="p-3 h-">
+                <div className="grid grid-cols-3 gap-4 mt-2">
+                    <div className="h-8 bg-gray-200 rounded animate-pulse">
+                    </div>
+                    <div className="h-8 bg-gray-200 rounded animate-pulse">
+                    </div>
+                    <div className="h-8 bg-gray-200 rounded animate-pulse">
+                    </div>
+                    <div className="h-8 col-span-2 bg-gray-200 rounded animate-pulse">
+                    </div>
+                    <div className="h-8 bg-gray-200 rounded  animate-pulse">
+                    </div>
+                    <div className="...">
+                    </div>
+                    <div className="col-span-2 ...">
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
