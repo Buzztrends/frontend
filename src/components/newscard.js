5159bf35dbe67574dcabb9803272b3f14db9cb3c
sps-master <siddhantsharma26072002@gmail.com>
2023-11-28 00:57:42 +0530

    card_changes

--- src/components/newscard.js
+++ src/components/newscard.js
@@ -6,31 +6,29 @@ const Newscard = ({data,toggleModal}) => {
 
 //   console.log(data);
   return (
-    <div className='flex flex-col w-[30%] border border-gray-100 shadow-sidd cursor-pointer' onClick={()=>{toggleModal({data: data })}}>
+    <div className='flex flex-col w-1/5 border border-gray-100 shadow-sidd cursor-pointer' onClick={()=>{toggleModal({data: data })}}>
         
         {data.top_image && <img alt="" className='h-44 object-cover' src={data.top_image}  />}
         <div className='flex flex-col gap-3 p-3 font-poppins'>
         <div className='flex flex-col '>
-            <div className=' text-base font-medium leading-7 tracking-wide'>
+            <div className=' text-sm font-medium leading-7 tracking-wide'>
              {data.title && data.title.length > 50
     ? `${data.title.slice(0, 50)}...` : data.title} {data.event_name && data.event_name.length > 50
     ? `${data.event_name.slice(0, 50)}...` : data.event_name}</div>
             <div className='text-xs font-normal leading-5 tracking-wide'>{data.source} </div>
         </div>
-        <div className='text-s font-normal leading-5 tracking-wide'> {data.description && data.description.length > 50
+        <div className='text-xs font-normal leading-5 tracking-wide'> {data.description && data.description.length > 50
     ? `${data.description.slice(0, 50)}...`
     : data.description}</div>
-        <div className='flex justify-between text-xs font-normal leading-5 tracking-wide'>
-            <button className='flex gap-1 items-center text-font-color'>
-                <LiaSave />
-                <span>save</span>
-            </button>
+    </div>
+        <div className='text-xs font-normal leading-5 tracking-wide mt-auto px-3 pb-3 ml-auto'>
+
             <div className='flex gap-1 items-center text-font-color' onClick={()=>document.getElementById('stats-modal').showModal()}>
-                <span>See Stats</span>
+                <span>Learn More</span>
                 <LiaGreaterThanSolid />
             </div>
         </div>
-    </div>
+    
     </div>
   )
 }
