b680bede5a9ff40ba4be83c29184729fcbd5964d
Rishabh Sinha <sinharishabh2002@gmail.com>
2023-11-28 01:54:54 +0530

    Insta icon

--- src/components/postfeatures.js
+++ src/components/postfeatures.js
@@ -49,11 +49,11 @@ const Postfeatures = () => {
       {/* <button className='w-1/3 h-20 rounded-lg border-2 border-gray-400 bg-gradient-linear text-white p-2 text-xl self-center'>Publish</button> */}
       {/* <Button buttonText={"Publish"} /> */}
       <div className='grid grid-cols-2 gap-x-10 gap-y-5'>
-        <button className='col-span-1 h-[49px] w-full rounded-[12px] border-2 border-purple text-center hover:bg-gradient-linear hover:text-white'>
+        <button className='col-span-1 h-[49px] w-full rounded-[12px] border-2 border-black text-center hover:bg-gradient-linear hover:text-white'>
           Save
         </button>
 
-        <button onClick={handleDownload} className='col-span-1 h-[49px] w-full rounded-[12px] border-2 border-purple flex items-center p-4 justify-between hover:bg-gradient-linear hover:text-white'>
+        <button onClick={handleDownload} className='col-span-1 h-[49px] w-full rounded-[12px] border-2 border-black flex items-center p-4 justify-between hover:bg-gradient-linear hover:text-white'>
           <span>Download</span>
           <div className='animate-bounce mx-2 mt-1'>
 
@@ -66,7 +66,7 @@ const Postfeatures = () => {
           <span>Save to Campaigns</span>
         </button> */}
 
-        <button className='col-span-1 h-[70px] w-full rounded-[12px] border-2 border-purple flex items-center p-4 justify-between hover:bg-gradient-linear hover:text-white'>
+        <button className='col-span-1 h-[70px] w-full rounded-[12px] border-2 border-black flex items-center p-4 justify-between hover:bg-gradient-linear hover:text-white'>
           <span>Send for<br></br> approval</span>
           <FaCheck />
         </button>
